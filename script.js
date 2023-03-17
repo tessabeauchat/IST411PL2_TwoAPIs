b5b2d84d77322574e9bc0bba3bec29111678af0b
Rachel McAdams <rxm5752@psu.edu>
2023-03-17 13:18:24 -0400

    delete unnecessary comment

--- script.js
+++ script.js
@@ -1,16 +1,9 @@
 let data;
 getMovieAPIData();
 
-// function getMovieAPIData(){
-//     const request = new XMLHttpRequest;
-//     request.open("GET","https://api.themoviedb.org/3/movie/550?", true);
-//     request.setRequestHeader('key','ea21b9adbbf424fd68259ea26cdb0591');
-// }
-
 function getMovieAPIData(){
     const request = new XMLHttpRequest;
     request.open("GET","https://api.themoviedb.org/3/discover/movie?api_key=ea21b9adbbf424fd68259ea26cdb0591", true);
-    // request.setRequestHeader('key','ea21b9adbbf424fd68259ea26cdb0591');
 
     request.onload = function() {
         data = JSON.parse(this.response);
